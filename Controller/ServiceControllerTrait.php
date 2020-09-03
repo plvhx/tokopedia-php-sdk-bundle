d79f7014fb9ea2886a0f0fd724a2fa0408ae5f5b
plvhx <gandung@slackerc0de.us>
2020-09-03 21:52:46 +0700

    Adding Controller/ServiceControllerTrait.php

---
+++ Controller/ServiceControllerTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gandung\Tokopedia\TokopediaBundle\Controller;
+
+use Gandung\Tokopedia\ServiceInterface;
+
+/**
+ * @author Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
+ */
+trait ServiceControllerTrait
+{
+	/**
+	 * @var ServiceInterface
+	 */
+	private $service;
+
+	/**
+	 * @param ServiceInterface $service
+	 * @return void
+	 */
+	public function __construct(ServiceInterface $service)
+	{
+		$this->setService($service);
+	}
+
+	/**
+	 * Get associated service object.
+	 *
+	 * @return ServiceInterface
+	 */
+	public function getService()
+	{
+		return $this->service;
+	}
+
+	/**
+	 * Set service object.
+	 *
+	 * @param ServiceInterface $service
+	 * @return void
+	 */
+	public function setService(ServiceInterface $service)
+	{
+		$this->service = $service;
+	}
+}
