a6a8760aaaa2465eca0b1c56d3b114c805473465
plvhx <gandung@slackerc0de.us>
2020-09-05 21:52:19 +0700

    Adding Tests/FetchServiceTest.php

---
+++ Tests/FetchServiceTest.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gandung\Tokopedia\TokopediaBundle\Tests;
+
+use Gandung\Tokopedia\ServiceFactory;
+use Gandung\Tokopedia\Auth\AuthorizationInterface;
+use Gandung\Tokopedia\Service\Campaign;
+use Gandung\Tokopedia\Service\Category;
+use Gandung\Tokopedia\Service\Interaction;
+use Gandung\Tokopedia\Service\Logistic;
+use Gandung\Tokopedia\Service\Order;
+use Gandung\Tokopedia\Service\Product;
+use Gandung\Tokopedia\Service\Shop;
+
+/**
+ * @author Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
+ */
+class FetchServiceTest extends AbstractTestCase
+{
+	use TestCaseTrait;
+
+	public function testCanGetAuthorizationObject()
+	{
+		$this->assertInstanceOf(
+			AuthorizationInterface::class,
+			$this->getContainer()->get(AuthorizationInterface::class)
+		);
+	}
+
+	public function testCanGetServiceFactoryObject()
+	{
+		$this->assertInstanceOf(
+			ServiceFactory::class,
+			$this->getContainer()->get(ServiceFactory::class)
+		);
+	}
+
+	public function testCanGetCampaignServiceObject()
+	{
+		$this->assertInstanceOf(
+			Campaign::class,
+			$this->getContainer()->get(Campaign::class)
+		);
+	}
+
+	public function testCanGetCategoryServiceObject()
+	{
+		$this->assertInstanceOf(
+			Category::class,
+			$this->getContainer()->get(Category::class)
+		);
+	}
+
+	public function testCanGetInteractionServiceObject()
+	{
+		$this->assertInstanceOf(
+			Interaction::class,
+			$this->getContainer()->get(Interaction::class)
+		);
+	}
+
+	public function testCanGetLogisticServiceObject()
+	{
+		$this->assertInstanceOf(
+			Logistic::class,
+			$this->getContainer()->get(Logistic::class)
+		);
+	}
+
+	public function testCanGetOrderServiceObject()
+	{
+		$this->assertInstanceOf(
+			Order::class,
+			$this->getContainer()->get(Order::class)
+		);
+	}
+
+	public function testCanGetProductServiceObject()
+	{
+		$this->assertInstanceOf(
+			Product::class,
+			$this->getContainer()->get(Product::class)
+		);
+	}
+
+	public function testCanGetShopServiceObject()
+	{
+		$this->assertInstanceOf(
+			Shop::class,
+			$this->getContainer()->get(Shop::class)
+		);
+	}
+}
