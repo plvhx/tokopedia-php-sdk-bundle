cd7f24d389432f8eb5f60849516d50f9bf9fe13f
plvhx <gandung@slackerc0de.us>
2020-09-05 21:52:47 +0700

    Adding Tests/TestCaseTrait.php

---
+++ Tests/TestCaseTrait.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gandung\Tokopedia\TokopediaBundle\Tests;
+
+/**
+ * @author Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
+ */
+trait TestCaseTrait
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public function setUp(): void
+	{
+		$this->setContainer($this->createContainer());
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function tearDown(): void
+	{
+	}
+}
